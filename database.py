b678a8c575f6daca186e5e6856a8f2183cc52b5d
SumitPanigrahiGit <sumitofficial1969@gmail.com>
2026-08-28 14:13:44 +0530

    Day 12: Dockerized the application with docker-compose

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:sumit2006@localhost:2000/taskdb"
+import os
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:YOUR_PASSWORD@localhost:5432/taskdb")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
